7c93c58e6b733c1520121494adb04952e5423ef2
ShinaHono <93893258+ShinaHono@users.noreply.github.com>
2022-08-08 09:03:22 +0900

    Add files via upload

--- style.js
+++ style.js
@@ -8,6 +8,10 @@ deviceType[0].addEventListener('touchstart', ()=>{
     deviceType[0].classList.add("smart");
     // ev();
 });
+deviceType[0].addEventListener('touchmove', ()=>{
+    deviceType[0].classList.add("smart");
+    // ev();
+});
 deviceType[0].addEventListener('touchend', ()=>{
     deviceType[0].classList.remove("smart");
 });
@@ -17,11 +21,17 @@ deviceType[0].addEventListener('touchstart', (event)=>{console.log(event)}
 deviceType[1].addEventListener('touchstart', ()=>{
     deviceType[1].classList.add("smart");
 });
+deviceType[1].addEventListener('touchmove', ()=>{
+    deviceType[0].classList.add("smart");
+});
 deviceType[1].addEventListener('touchend', ()=>{
     deviceType[1].classList.remove("smart");
 });
 
 deviceType[2].addEventListener('touchstart', ()=>{
+    deviceType[2].classList.add("smart");
+});
+deviceType[2].addEventListener('touchmove', ()=>{
     deviceType[2].classList.add("smart");
 });
 deviceType[2].addEventListener('touchend', ()=>{
@@ -31,11 +41,17 @@ deviceType[2].addEventListener('touchend', ()=>{
 deviceType[3].addEventListener('touchstart', ()=>{
     deviceType[3].classList.add("smart");
 });
+deviceType[3].addEventListener('touchmove', ()=>{
+    deviceType[3].classList.add("smart");
+});
 deviceType[3].addEventListener('touchend', ()=>{
     deviceType[3].classList.remove("smart");
 });
 
 deviceType[4].addEventListener('touchstart', ()=>{
+    deviceType[4].classList.add("smart");
+});
+deviceType[4].addEventListener('touchmove', ()=>{
     deviceType[4].classList.add("smart");
 });
 deviceType[4].addEventListener('touchend', ()=>{
@@ -45,6 +61,9 @@ deviceType[4].addEventListener('touchend', ()=>{
 deviceType[5].addEventListener('touchstart', ()=>{
     deviceType[5].classList.add("smart");
 });
+deviceType[5].addEventListener('touchmove', ()=>{
+    deviceType[5].classList.add("smart");
+});
 deviceType[5].addEventListener('touchend', ()=>{
     deviceType[5].classList.remove("smart");
 });
@@ -52,11 +71,17 @@ deviceType[5].addEventListener('touchend', ()=>{
 deviceType[6].addEventListener('touchstart', ()=>{
     deviceType[6].classList.add("smart");
 });
+deviceType[6].addEventListener('touchmove', ()=>{
+    deviceType[6].classList.add("smart");
+});
 deviceType[6].addEventListener('touchend', ()=>{
     deviceType[6].classList.remove("smart");
 });
 
 deviceType[7].addEventListener('touchstart', ()=>{
+    deviceType[7].classList.add("smart");
+});
+deviceType[7].addEventListener('touchmove', ()=>{
     deviceType[7].classList.add("smart");
 });
 deviceType[7].addEventListener('touchend', ()=>{
